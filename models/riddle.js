d44f99e0c6c2d0716df7a6f176c21f0f0c30a99f
pedrotroncoso28 <PEDRO.TRONCOSOJIMENEZ02@login.cuny.edu>
2025-12-15 03:18:11 -0500

    Adjusting riddles (settings)

---
+++ models/riddle.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ResponseSchema = new mongoose.Schema({
+  answerText: {
+    type: String,
+    required: true
+  },
+  isCorrect: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const RiddleSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  difficulty: {
+    type: String,
+    enum: ['easy', 'medium', 'hard'],
+    required: true
+  },
+  correctAnswer: {
+    type: String,
+    required: true
+  },
+  responses: [ResponseSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Riddle', RiddleSchema);
