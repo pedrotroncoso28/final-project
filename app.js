44a10d3ba39abe2093c797686fa07793d825b554
pedrotroncoso28 <PEDRO.TRONCOSOJIMENEZ02@login.cuny.edu>
2025-12-15 03:08:44 -0500

    Connecting to mongodb

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+mongoose.connect(
+  'mongodb+srv://cluenityUser:cluenity123@cluster0.bnu01mv.mongodb.net/cluenity'
+);
+
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
