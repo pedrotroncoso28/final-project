3e5b6bd0088178a680715932940645089374460c
pedrotroncoso28 <PEDRO.TRONCOSOJIMENEZ02@login.cuny.edu>
2025-12-18 08:58:53 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Public Archive', page: 'home' });
+  res.render('index', { title: 'Public Archive' });
 });
 
 /* GET about page */
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'About Us', page: 'about'});
+  res.render('about', { title: 'About Us' });
 });
 
 module.exports = router;
